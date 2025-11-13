f63d0ce46259df38bc02565ce3a10b22523a8704
Jhoan Muñoz <jhoanmunozman@gmail.com>
2025-11-12 20:15:59 -0500

    Swagger agregado

---
+++ src/main/java/com/cursos/api/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.cursos.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
